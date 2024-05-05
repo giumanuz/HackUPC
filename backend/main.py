a8832bfc8abb4c345e772093384de62a5f1e5208
giumanuz <giuliomanuzzi@gmail.com>
2024-05-05 02:37:27 +0200

    chore: Refactor file upload path in backend code

--- backend/main.py
+++ backend/main.py
@@ -36,52 +36,18 @@ def upload_file():
     os.makedirs(FOLDER_PATH, exist_ok=True)
     files = request.files.getlist('file')
     getLogger().warn(files)
-    # for file in files:
-    #     ispdf = file.filename.endswith('.pdf')
-    #     istxt = file.filename.endswith('.txt')
-    #     if not (ispdf or istxt):
-    #         abort(400, 'Invalid file format. Only PDF and TXT files are allowed.')
 
-    # start_time = datetime.now()
     for file in files:
-        getLogger().warn(file)
         file_path = os.path.join(FOLDER_PATH, file.filename)
-        getLogger().warn(file_path)
         file.save(file_path)
-        getLogger().warn([f for f in os.listdir(FOLDER_PATH)])
 
-
-
-        
-    #     ispdf = file.filename.endswith('.pdf')
-    #     istxt = file.filename.endswith('.txt')
-    #     file_path = os.path.join(FOLDER_PATH, file.filename)
-    #     getLogger().warn(file_path)
-    #     text = read_file(file_path)
-    #     getLogger().warn(text)
-    #     with open(f'{FOLDER_PATH}/{file.filename[:-4]}.txt', 'w') as f:
-    #         f.write(text)
-    # end_time = datetime.now()
-    # print(f"LOG upload: Tempo di risposta: {end_time - start_time}")
 
     current_time = int(time())
-    start_time = datetime.now()
     query_engine = return_query_engine(f"basta {current_time}")
-    end_time = datetime.now()
-    print(f"LOG train: Tempo di risposta: {end_time - start_time}")
+
     NUMBER_CURRENT_CHAT +=1
     HISTORY_CHAT[NUMBER_CURRENT_CHAT] = []
     return ('', 200)
-
-
-# @app.route('/delete_file', methods=['POST', 'GET'])
-# def delete_file():
-#     if request.method == 'POST':
-#         file_name = request.form['fileToDelete']
-#         file_path = f'file_uploadati/{file_name}'
-#         os.remove(file_path)
-#         return redirect(url_for('home'))
-#     return render_template('index.html')
 
 @app.route('/query', methods=['POST'])
 def query():
